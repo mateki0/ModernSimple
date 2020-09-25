9050b6155f00beabbf986d38743da12cd1756428
mateki0 <mateki0@interia.pl>
2020-09-25 19:52:44 +0200

    pages changed

--- src/pages/o-mnie.tsx
+++ src/pages/o-mnie.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import * as React from 'react';
 
-const About = () => {
-  return <div>Hello</div>
-}
+const About: React.FC = () => {
+  return <div>Hello</div>;
+};
 
-export default About
+export default About;
