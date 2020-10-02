df3582cab3b53f1fa0b2f4d961730f27c373c457
mateki0 <mateki0@interia.pl>
2020-10-02 07:23:19 +0200

    added babelrc config

--- src/components/Footer/styled/ContentSpan.tsx
+++ src/components/Footer/styled/ContentSpan.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ContentSpan = styled.span`
   font-size: 6px;
   color: #2c9692;
-  text-decoration: none;
+
   @media only screen and (min-width: 1024px) {
     font-size: 14px;
   }
