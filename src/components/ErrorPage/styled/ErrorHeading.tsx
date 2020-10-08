de5ead635da3eaae9db6e0b0dadb5a256a704a52
mateki0 <mateki0@interia.pl>
2020-10-08 21:52:56 +0200

    404 page added

---
+++ src/components/ErrorPage/styled/ErrorHeading.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ErrorHeading = styled.h1`
+  font-size: 21px;
+  font-weight: bold;
+
+  @media only screen and (min-width: 1024px) {
+    font-size: 42px;
+  }
+`;
+export default ErrorHeading;
---
+++ src/components/ErrorPage/styled/ErrorText.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ErrorText = styled.p`
+  font-size: 13px;
+  color: #fff;
+  text-align: center;
+  @media only screen and (min-width: 1024px) {
+    font-size: 26px;
+  }
+`;
+export default ErrorText;
