7f8c566206796e7964fd9baa97915203eb27fb90
mateki0 <mateki0@interia.pl>
2020-10-13 22:39:44 +0200

    padding fixed

--- src/components/AboutImg/styled/AboutWrapper.tsx
+++ src/components/AboutImg/styled/AboutWrapper.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const AboutWrapper = styled.div`
-  padding: 30px 0 23px 5px;
+  padding: 23px 0 23px 5px;
   display: flex;
   border-bottom: 2px solid #eca72c;
   @media only screen and (min-width: 1024px) {
--- src/components/OfferBox/styled/OfferWrapper.tsx
+++ src/components/OfferBox/styled/OfferWrapper.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const OfferWrapper = styled.div`
-  padding-bottom: 23px;
-  padding-top: 30px;
+  padding: 23px 0;
+
   width: calc(100% + 2 * 9px);
   margin-left: -9px;
   @media only screen and (min-width: 1024px) {
