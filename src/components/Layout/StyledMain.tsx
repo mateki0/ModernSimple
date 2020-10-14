1c3521a56d1e327f64ee84b7bd70f2a96a24b9bc
mateki0 <mateki0@interia.pl>
2020-10-14 08:25:02 +0200

    margin-top changed

--- src/components/Layout/StyledMain.tsx
+++ src/components/Layout/StyledMain.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 const StyledMain = styled.main`
-  margin-top: 55px;
+  margin-top: 65px;
   @media only screen and (min-width: 1024px) {
     margin-top: 134px;
   }
