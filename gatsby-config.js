0fbfb72a4e8bdd81f2ff1093f4e4b84a0a6fa064
mateki0 <mateki0@interia.pl>
2020-10-18 13:04:46 +0200

    removed unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,7 +97,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 906,
+              maxWidth: 1920,
             },
           },
         ],
