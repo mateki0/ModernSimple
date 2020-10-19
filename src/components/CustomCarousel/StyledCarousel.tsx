2d5da7bdf4be577e8357ec3bcec7c3081aee6412
mateki0 <mateki0@interia.pl>
2020-10-19 08:33:35 +0200

    slider control flex wrap added

--- src/components/CustomCarousel/StyledCarousel.tsx
+++ src/components/CustomCarousel/StyledCarousel.tsx
@@ -2,10 +2,21 @@ import styled from 'styled-components';
 import Carousel from 'nuka-carousel';
 
 const StyledCarousel = styled(Carousel)`
+
 &.slider {
   .slider-control-centerleft,
   .slider-control-centerright {
     opacity: 0;
+  }
+  .slider-control-bottomcenter{
+      width:100%;
+      
+  }
+  .slider-control-bottomcenter ul{
+    width:100%;
+    justify-content:center;
+    flex-wrap:wrap;
+    
   }
 }
 &:hover {
