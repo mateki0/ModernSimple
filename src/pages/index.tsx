940de4a6e278b1c9ae797007dedc2046c6e4e8e0
mateki0 <mateki0@interia.pl>
2021-01-04 10:42:13 +0100

    black week modal deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,11 @@ import SEO from '../components/seo';
 import OfferBox from '../components/OfferBox';
 import Cooperation from '../components/Cooperation';
 import LastProjects from '../components/LastProjects';
-import PromoModal from '../components/PromoModal';
 
 const IndexPage: React.FC = () => (
   <Layout>
     <SEO title="Strona główna" />
     <OfferBox />
-    <PromoModal />
     <Cooperation />
     <LastProjects />
   </Layout>
